228accd69d128e77408867cb540edcd1808f97b0
mcmaquine <mcmaquine@gmail.com>
2022-04-04 08:18:55 -0400

    added electronic gear ratio and other functions

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from MR_JE_C import *
 from pymodbus.client.sync import *
-import utils
+
 
 def main():
     client = ModbusTcpClient('10.8.0.201')
     client.connect()
 
-    J1 = MR_JE_C( cli = client )
+    J1 = MR_JE_C(cli=client)
 
     print(J1.get_actual_position())
 
     client.close()
+
 
 if __name__ == '__main__':
     main()
