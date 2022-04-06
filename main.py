8f6c62a9241e78057de57e45eb284524a64b0b49
mcmaquine <mcmaquine@gmail.com>
2022-04-06 17:06:33 -0400

    dealing with negative numbers

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+from ctypes import sizeof
 from MR_JE_C import *
-from pymodbus.client.sync import *
 from pyModbusTCP.client import ModbusClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.payload import BinaryPayloadBuilder
+from pymodbus.compat import iteritems
+
 
 
 def main():
     c = ModbusClient(host='10.8.0.201', port=502, unit_id=255)
     c.open()
 
-    result = c.read_holding_registers(index['MR_MODES_OPERATION_DISPLAY'], 1)
-    print(result[0])
+    result = c.read_holding_registers(index['MR_POSITION_ACTUAL_VALUE'], 2)
+    print('Return size '+str(len(result)))
+    print('Value read'+str(result))
+    decoder = BinaryPayloadDecoder.fromRegisters(result, byteorder=Endian.Big)
+
+    #print(decoder.decode_8bit_int())
+
+    uint8_list = []
+    for data in range(len(result)):
+        print( data )
+        decoder.skip_bytes(1)
+        uint8_list.append( decoder.decode_8bit_int())
+
+    print("-" * 60)
+    print("Decoded Data")
+    print("-" * 60)
+
+    print( uint8_list )
+    
     #client = ModbusTcpClient('10.8.0.201')
     #client.connect()
 
